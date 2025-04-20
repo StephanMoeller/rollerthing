1413814f0fa2d69e7d23e2e9bbb759208c8dcabd
Stephan Møller <stephanryer@hotmail.com>
2025-04-20 16:38:30 +0200

    latest

--- lua/rollerthing/init.lua
+++ lua/rollerthing/init.lua
@@ -1,5 +1,15 @@
-local export = {}
-local function export.roll_go(){
-  vim.cmd('echo "hello"')
-}
-return export
+local M = {} -- M stands for module, a naming convention
+
+function M.setup()
+   print("hello")
+  -- Create a command, ':DoTheThing'
+  vim.api.nvim_create_user_command(
+    'DoTheThing',
+    function(input)
+        print "Something should happen here..."
+    end,
+    {bang = true, desc = 'a new command to do the thing'}
+)
+end
+
+return M
